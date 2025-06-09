ada7eebfaecda46c09b2c0a7fa0b41613a36faf9
Matěj Ujhelský <matej.ujhelsky@student.sps-cl.cz>
2025-06-09 10:25:50 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -5,12 +5,19 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // API cesty 
 const apiRoutes = require('./api/api');
-app.use('/api', apiRoutes); // /api/items
+app.use('/api', apiRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
+}).on('error', (err) => {
+  if (err.code === 'EADDRINUSE') {
+    console.error(`Port ${port} is already in use. Try another port.`);
+  } else {
+    console.error('Error starting server:', err);
+  }
 });
